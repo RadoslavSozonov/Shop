71ca4d96ae47c172f8e3155def6858db817b7e06
RadoslavSozonov <radoslav.sozonov@mail.bg>
2021-06-27 18:15:36 +0300

    refactoring the class
    refactoring the class

--- src/Cashier.java
+++ src/Cashier.java
@@ -9,26 +9,38 @@ public class Cashier {
     }
 
     public void printReceipt(Cart cart, Date date, Time time){
+        Double[] arrayOfSumAndDiscount = new Double[2];
+        beginningOfReceipt(date,time);
+        arrayOfSumAndDiscount=bodyOfReceipt(cart,date);
+        endOfReceipt(arrayOfSumAndDiscount[0],arrayOfSumAndDiscount[1]);
+    }
+
+    public void beginningOfReceipt(Date date, Time time){
+        System.out.println("Date: "+date.toString()+" "+time.toString());
+        System.out.println("");
+        System.out.println("---Products---\n");
+    }
+
+    public Double[] bodyOfReceipt(Cart cart, Date date){
+        Double[] arrayOfSumAndDiscount = new Double[2];
         double sum=0;
         double discountSum=0;
         String[] information;
-        beginningOfReceipt(date,time);
         for(Purchase purchase :cart.getList()){
             information=catalog.readPurchase(purchase,date);
             System.out.println(information[0]);
             sum+= Double.parseDouble(information[1]);
             discountSum+= Double.parseDouble(information[2]);
         }
+        arrayOfSumAndDiscount[0]=sum;
+        arrayOfSumAndDiscount[1]=discountSum;
+        return arrayOfSumAndDiscount;
+    }
 
+    public void endOfReceipt(double sum, double discountSum){
         System.out.println("-----------------------------------------------------------------------------------");
         System.out.println("SUBTOTAL: $"+sum);
         System.out.println("DISCOUNT: -$"+discountSum+"\n");
         System.out.println("TOTAL: $"+(sum-discountSum));
-    }
-
-    public void beginningOfReceipt(Date date, Time time){
-        System.out.println("Date: "+date.toString()+" "+time.toString());
-        System.out.println("");
-        System.out.println("---Products---\n\n");
     }
 }
