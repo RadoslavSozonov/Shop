57b520e40009ff7448804f0debd94776ed930b15
RadoslavSozonov <radoslav.sozonov@mail.bg>
2021-06-27 20:52:24 +0300

    Add descriptions
    Add javadoc and comments to the classes

--- src/Cart.java
+++ src/Cart.java
@@ -4,18 +4,34 @@ import java.util.List;
 public class Cart {
     List<Purchase> list;
 
+    /**
+     * Constructor of the class
+     */
     public Cart(){
         list = new ArrayList<>();
     }
 
+    /**
+     * Add a new purchase to the list
+     * @param purchase - the new purchase
+     * @return - true if the purchase is added successfully
+     */
     public boolean add(Purchase purchase){
         return list.add(purchase);
     }
 
+    /**
+     * Get all purchases
+     * @return - all purchases
+     */
     public List<Purchase> getList() {
         return list;
     }
 
+    /**
+     * Set a new list of purchases
+     * @param list - list of purchases
+     */
     public void setList(List<Purchase> list) {
         this.list = list;
     }
--- src/Purchase.java
+++ src/Purchase.java
@@ -1,14 +1,26 @@
 public class Purchase {
     private String description;
 
+    /**
+     * Constructor of the class
+     * @param description - description of the purchase
+     */
     public Purchase(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the description of the purchase
+     * @return - the description of the purchase
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set a new description of the purchase
+     * @param description - description of the purchase
+     */
     public void setDescription(String description) {
         this.description = description;
     }
